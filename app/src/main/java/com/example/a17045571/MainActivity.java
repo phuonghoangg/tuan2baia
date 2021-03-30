f3eadda6697c082f06b8cb8848459ce6eedec0b2
phuonghoangg <hoangphuong291199@gmail.com>
2021-03-30 22:46:37 +0700

    add new project

---
+++ app/src/main/java/com/example/a17045571/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a17045571;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
